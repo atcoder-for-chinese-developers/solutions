2453c96059af05a970fa1f952730f38b9df67c52
psz2007 <63392367+psz2007@users.noreply.github.com>
2022-11-02 09:34:33 +0800

    upload sols

--- .github/workflows/gen.js
+++ .github/workflows/gen.js
@@ -2,22 +2,22 @@ const fs = require('fs');
 const yamlFront = require('yaml-front-matter');
 
 module.exports = ({ github, context, core }) => {
-    let list = fs.readdirSync('.');
-    let data = {};
-    list.forEach(file => {
-        let stats = fs.statSync(file);
-        if (stats.isFile()) {
-            let parts = file.split('.');
-            let extName = parts.slice(-1)[0];
-            if (extName === 'md' && parts.length === 4) {
-                let contest = parts[0], problem = parts[1], ID = parts[2];
-                data[contest] = data[contest] || {};
-                data[contest][problem] = data[contest][problem] || {};
-                data[contest][problem][ID] = yamlFront.loadFront(fs.readFileSync(file));
-                fs.writeFileSync(file, data[contest][problem][ID].__content.trim());
-                delete data[contest][problem][ID].__content;
-            }
-        }
-    });
-    fs.writeFileSync('list.json', JSON.stringify(data));
+	let list = fs.readdirSync('.');
+	let data = {};
+	list.forEach(file => {
+		let stats = fs.statSync(file);
+		if (stats.isFile()) {
+			let parts = file.split('.');
+			let extName = parts.slice(-1)[0];
+			if (extName === 'md' && parts.length === 4) {
+				let contest = parts[0], problem = parts[1], ID = parts[2];
+				data[contest] = data[contest] || {};
+				data[contest][problem] = data[contest][problem] || {};
+				data[contest][problem][ID] = yamlFront.loadFront(fs.readFileSync(file));
+				fs.writeFileSync(file, data[contest][problem][ID].__content.trim());
+				delete data[contest][problem][ID].__content;
+			}
+		}
+	});
+	fs.writeFileSync('list.json', JSON.stringify(data));
 }
