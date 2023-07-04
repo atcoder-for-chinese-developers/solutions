1698d54869505520cca82435e56a6220c37a38ae
e3c8f1a924 <46619411+e3c8f1a924@users.noreply.github.com>
2023-07-04 15:34:43 +0800

    add: Cloudflare Pages headers

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-bash util/getLastCommit.sh
-node build.js
-rm -rf commitInfo
+bash util/getLastCommit.sh;
+node build.js;
+rm -rf commitInfo;
+
+if [ -d $STATIC_DIR -a $STATIC_DIR ]; then
+    \cp -r $STATIC_DIR/* dist
+fi;
